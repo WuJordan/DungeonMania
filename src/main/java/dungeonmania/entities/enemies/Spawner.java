8019ebc4f8e7c5aad108d21d4e77d11b24b89945
Jordan Wu <114432052+WuJordan@users.noreply.github.com>
2024-03-31 01:56:34 +1100

    Delete src directory

--- src/main/java/dungeonmania/entities/enemies/Spawner.java
+++
@@ -1,6 +0,0 @@
-package dungeonmania.entities.enemies;
-
-// Public interface for Spawners
-public interface Spawner {
-
-}
--- src/main/java/dungeonmania/goals/GoalNode.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.goals;
-
-import dungeonmania.Game;
-
-public interface GoalNode {
-    public boolean achieved(Game game);
-
-    public String toString(Game game);
-}
